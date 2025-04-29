f3fa7dca798a061c347ba9c611257d9272811c33
CaY-2 <cay.taugner@drvbund.onmicrosoft.com>
2025-04-29 10:32:01 +0200

    Overlay Class

--- Kalkulator/src/Kalkulator/Matrix.java
+++ Kalkulator/src/Kalkulator/Matrix.java
@@ -4,17 +4,16 @@ public class Matrix {
 
 	protected double[][] matrix;
 
+	@Override
+	public String toString() {
+		String ausgabe = "";
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				ausgabe += matrix[i][j] + "\t";
+			}
+			ausgabe += "\n";
+		}
 
-    @Override
-    public String toString() {
-    String ausgabe="";
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-            ausgabe+=matrix[i][j]+"\t";
-            }
-            ausgabe+="\n";
-        }
-
-        return ausgabe;
-    }
+		return ausgabe;
+	}
 }
